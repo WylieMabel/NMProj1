aabf591334988e3859648fbceb1716f3af3adbb1
Vulpen0 <spisani.gabriele@gmail.com>
2024-10-20 16:34:50 +0200

    Prep for task 3

--- MHSim.R
+++ MHSim.R
@@ -20,21 +20,17 @@ computeStats = function(net)
   nvertices <- nrow(net) 
   
   # Edge count
-  # Unit vector of size equal to matix row count
-  unit = rep(1,nvertices);
   # Not yet correct must remove reciprocal edges as they got double counted
-  stat1 = sum((net %*% unit) * unit);
+  stat1 = sum(net);
   
-  # Matrix representing reciprocal ties. Warning, Hadamard product not matrix prod.
-  recip = net * t(net)
-  # Symmetric matrix, /2 to avoid double count
-  stat2 = sum((recip %*% unit) * unit) / 2;
+  # Sum of matrix representing reciprocal ties. Warning, Hadamard product not matrix prod.
+  stat2 = sum(net * t(net)) / 2;
   # Now stat1 is correct
   stat1 = stat1 - stat2;
   
   # Vector where each row is the indegree of the correspoding node
   # trnaspose is necessary to ensure each row is a receiver instead of a sender
-  indegree = as.vector(t(net) %*% unit);
+  indegree = as.vector(t(net) %*% rep(1,dim(net)[1]));
   # New vector that filters only indegrees of 2
   star_2 = ifelse(indegree[1:nvertices] == 2, 1, 0);
   stat3 = sum(star_2);
@@ -51,18 +47,11 @@ computeStats = function(net)
 #' edge, mutual and nodematch statistics
 #'
 #' @param net an object of class `matrix`. Adjacency matrix of the network.
-#' @param theta1 a numeric value. The value of the edge parameter of the ERGM.
-#' @param theta2 a numeric value. The value of the mutual parameter of the ERGM.
-#' @param theta3 a numeric value. The value of the istar(2) parameter of the ERGM.
+#' @param theta statistics vector, for task 2 this are the following values:
+#' edge count, reciprocal tie count, and 2-istar count.
 #'
 #' @return next state of the Markov Chain
-#'
-#' @examples
-#' MHstep(
-#'   matrix(c(0, 1, 0, 0, 0, 0, 1, 1, 0), nrow = 3, ncol = 3),
-#'   -log(0.5), log(0.4), log(.8)
-#' )
-MHstep <- function(net, theta1, theta2, theta3){
+MHstep <- function(net, theta){
   
   # Number of vertices in the network
   nvertices <- nrow(net) 
@@ -77,7 +66,6 @@ MHstep <- function(net, theta1, theta2, theta3){
   
   # Compute the change statistics
   
-  theta = c(theta1,theta2,theta3);
   current_stat = computeStats(net);
   new_stat = computeStats(new_net);
   delta_stat = new_stat - current_stat;
@@ -108,9 +96,8 @@ MHstep <- function(net, theta1, theta2, theta3){
 #' edge, mutual and nodematch statistics
 #'
 #' @param net an object of class `matrix`. Adjacency matrix of the network.
-#' @param theta1 a numeric value. The value of the edge parameter of the ERGM.
-#' @param theta2 a numeric value. The value of the mutual parameter of the ERGM.
-#' @param theta3 a numeric value. The value of the istar(2) parameter of the ERGM.
+#' @param theta statistics vector, for task 2 this are the following values:
+#' edge count, reciprocal tie count, and 2-istar count.
 #' @param burnin an integer value.
 #'   Number of steps to reach the stationary distribution.
 #' @param thinning an integer value. Number of steps between simulated networks.
@@ -119,25 +106,20 @@ MHstep <- function(net, theta1, theta2, theta3){
 #' @return a named list:
 #'   - netSim: an `array` with the adjancency matrices of the simulated networks.
 #'   - statSim: a `matrix` with the value of the statistic defining the ERGM.
-#'
-#' @examples
-#' MarkovChain(
-#'   matrix(c(0, 1, 0, 0, 0, 0, 1, 1, 0), nrow = 3, ncol = 3),
-#'   -log(0.5), log(0.4), log(.8)
-#' )
 MarkovChain <- function(
     net,
-    theta1, theta2, theta3,
+    theta,
     burnin = 10000, thinning = 1000, nNet = 1000){
   
   # Burnin phase: repeating the steps of the chain "burnin" times  
   nvertices <- nrow(net)
   burninStep <- 1 # counter for the number of burnin steps
   
+  net_t = net;
   # Perform the burnin steps
   for (i in 1:burnin)
   {
-    net = MHstep(net,theta1,theta2,theta3);
+    net_t = MHstep(net_t,theta);
   }
   
   # After the burnin phase we draw the networks
@@ -150,8 +132,8 @@ MarkovChain <- function(
   
   for(i in 1:nNet)
   {
-    net = MHstep(net,theta1,theta2,theta3);
-    netSim[,,i] = net;
+    net_t = MHstep(net_t,theta);
+    netSim[,,i] = net_t;
     statSim[i,] = computeStats(net_t);
   }
   
