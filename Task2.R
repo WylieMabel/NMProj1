ea74f6319e4d9a40356a1a8f64fa8546671e7eed
Vulpen0 <spisani.gabriele@gmail.com>
2024-10-20 15:59:40 +0200

    Clean up of task 2.2, and prep for task 3

--- Task2.R
+++ Task2.R
@@ -1,50 +1,65 @@
 # task_2_2 ------------------------------------------------------------------
 #' Executes task 2.2
 #'
-#' @param n number of vertices
 #' @param advice advice adiancency matrix
-#' @param friendship friendship adiancency matrix
-#' @param report_to report_to adiancency matrix
 #'
-#' @return nothing, it just prints out the results at the moment, it first prints
-#' out the observed stats (edges,reciprocal,star2 in this order), then it outputs
-#' the average of those stats across all the simulated networks, right after stat
-#' the corrsponding p value is also printed out.
-#' TODO better output formatting
-task_2_2 = function(n, advice, friendship, report_to)
+#' @return nothing, it just prints out the evaluated statistics (see evalStat)
+task_2_2 = function(advice)
 {
   iterations = 1000
-  labels = c("ADVICE","FRIENDSHIP","REPORTS_TO")
-  simulate = list(advice,friendship,report_to)
   
-  for(i in 1:length(simulate))
+  obs_stats = computeStats(advice)
+  n = dim(advice)[1]
+  
+  result = MarkovChain(matrix(0,n,n),-2.76,0.68,0.05, nNet = iterations)
+  
+  out = evalStat(result$statSim,obs_stats)
+  print(out)
+}
+
+# evalStat ------------------------------------------------------------------
+#' Calculates the mean, standard deviation, p_values, and t_ratios from a
+#' given matrix of statistics vectors, each row should be a statistic from a
+#' simulated network.
+#'
+#' @param stats the statistics matrix collected from the simulation
+#' @param observed the statistics observed in the real network (should be a vector)
+#'
+#' @return a data frame containing the following columns in this order: observed,
+#' mean, std_dev, p_value, t_ratio
+evalStat = function(stats, observed)
+{
+  iter = dim(stats)[1];
+  s = dim(stats)[2];
+  mean = vector(length = s)
+  dev = vector(length = s)
+  p = vector(length = s)
+  t = vector(length = s)
+  
+  #Calculate mean and p value
+  for(i in 1:iter)
   {
-    obs_stats = computeStats(simulate[[i]])
-    print(c("OBSERVED",labels[i],obs_stats[1],obs_stats[2],obs_stats[3]))
-    result = MarkovChain(matrix(0,n,n),-2.76,0.68,0.05, nNet = iterations)
-    sim_edges = 0
-    p_edges = 0
-    sim_reciprocal = 0
-    p_reciprocal = 0
-    sim_star_2 = 0
-    p_star_2 = 0
-    for(j in 1:iterations)
+    st = stats[i,];
+    for(j in 1:s)
     {
-      stat = result$statSim[j,]
-      sim_edges = sim_edges + stat[1]
-      if(obs_stats[1] <= stat[1]) p_edges = p_edges + 1
-      sim_reciprocal = sim_reciprocal + stat[2]
-      if(obs_stats[2] <= stat[2]) p_reciprocal = p_reciprocal + 1
-      sim_star_2 = sim_star_2 + stat[3]
-      if(obs_stats[3] <= stat[3]) p_star_2 = p_star_2 + 1
+      stat = st[j];
+      mean[j] = mean[j] + stat;
+      if(observed[j] <= stat) p[j] = p[j] + 1;
     }
-    sim_edges = sim_edges / iterations
-    p_edges = p_edges / iterations
-    sim_reciprocal = sim_reciprocal / iterations
-    p_reciprocal = p_reciprocal / iterations
-    sim_star_2 = sim_star_2 / iterations
-    p_star_2 = p_star_2 / iterations
-    
-    print(c("SIMULATED",labels[i],sim_edges,p_edges,sim_reciprocal,p_reciprocal,sim_star_2,p_star_2))
   }
+  mean = mean / iter;
+  p = p / iter;
+  
+  #Calculate standard deviation
+  for(i in 1:iter)
+  {
+    st = stats[i,];
+    for(j in 1:s) dev[j] = dev[j] + abs(mean[j] - st[j])^2;
+  }
+  dev = sqrt(dev / iter);
+  
+  #Calculate t-ratios
+  for(i in 1:s) t[i] = abs(observed[i] - mean[i]) / dev[i];
+  
+  return(data.frame(observed = observed, mean = mean, std_dev = dev, p_value = p, t_ratio = t))
 }
